0473316b0febfc97700e41c489a7aeb893f132e0
KoltsovIV <koltsov273.15@gmail.com>
2021-12-20 11:14:19 +0200

    slick slider left, bootstrap and tiny sliders - deleted

--- src/js/script.js
+++ src/js/script.js
@@ -17,38 +17,3 @@ $(document).ready(function(){
       ]
     });
   });
-// *************************************************************************
-
-// // Tiny Slider 2
-
-// const slider = tns({
-//   container: '.carousel__inner',
-//   items: 1,
-//   slideBy: 'page',
-//   autoplay: false,
-//   controls: false,
-//   nav: false,
-//   responsive: {
-//     640: {
-//       edgePadding: 20,
-//       gutter: 20,
-//       items: 1
-//     },
-//     700: {
-//       gutter: 30
-//     },
-//     992: {
-//       autoWidth: true,
-//       items: 1
-//     }
-//   }
-// });
-
-// document.querySelector('.prev').addEventListener('click', function () {
-//   slider.goTo('prev');
-// });
-
-// document.querySelector('.next').addEventListener('click', function () {
-//   slider.goTo('next');
-// });
-// ***********************************************************
